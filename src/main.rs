195878fc2cfe5511b4ea25f08908bfcba63def6d
Vilhelm Bergsøe <vilhelmbergsoe@gmail.com>
2023-03-13 22:25:42 +0100

    moved address to 0.0.0.0:
    moved listening address to 0.0.0.0 instead of 127.0.0.1, so other
    machines can access the service

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ async fn main() -> Result<()> {
         .nest_service("/assets", ServeDir::new("assets"))
         .fallback(get(handle_404));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.layer(TraceLayer::new_for_http()).into_make_service())
